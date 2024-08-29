627a487626df6f634097921f8f493ff694180592
kristins08 <118137912+kristins08@users.noreply.github.com>
2024-08-29 11:23:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,29 +5,12 @@
 using namespace std;
 
 int main() {
-    // DO NOT MODIFY THIS CODE!!!
     int m, n;
     cin >> m;
     cin >> n;
     int values[m][n];
-    copy_n(istream_iterator<int>(cin), m * n, values[0]);
-    // END DO NOT MODIFY
-    // TODO Read in value to find
-    // TODO Find the number of occurrences of that value within the 2dArray `values'
-    /* 2dArray `values' has dimensions `m' and `n'
-     * Ex.
-     * m = 2
-     * n = 3
-     * values = {
-     *     {15, 20, 18},
-     *     {20, 32, 54}
-     * }
-     *
-     * If the value `20' were entered
-     * Your program should produce the output 2
-     */
+    copy_n(istream_iterator<int>(cin), m * n, values[0])
 
-    //Your code goes Here
     int countNumber;
     cin >> countNumber;
     int occurences = 0;
@@ -42,5 +25,5 @@ int main() {
     cout<< countNumber << " occurs " << occurences << " times." << endl;
     
     return 0; 
-}    //end of main function
+}    
 
